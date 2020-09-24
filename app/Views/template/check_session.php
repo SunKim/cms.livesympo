ad8a5049e29ad69538bba881dc79ccb47caf2159
SunKim <sjmarine97@gmail.com>
2020-09-24 22:34:41 +0900

    ing

--- app/Views/template/check_session.php
+++ app/Views/template/check_session.php
@@ -1,5 +1,5 @@
 <?php
-	if ( !isset($session['logged_in']) || $session['logged_in'] != 1) {
+	if ( !isset($session['admSeq']) || $session['admSeq'] <= 0) {
 		echo "<script>alert('로그인 정보가 없거나 만료되었습니다.\\n로그인 화면으로 이동합니다.');location.href='/login';</script>";
 	} else {
 		// echo "<script>alert('로그인 세션존재.');</script>";
